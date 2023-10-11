4b00629bd5310e5c5ed796012a77b5105ee07278
Russell Trow <russell@dockleafdigital.com>
2023-10-11 10:59:48 +0100

    Started migrating to Docusarus

---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+layout: default
+title: Oversight Committee (OC)
+nav_order: 1
+has_children: true
+has_toc: false
+---
+[//]: # (SPDX-License-Identifier: CC-BY-4.0)
+
+# The Green Software Foundation Oversight Committee
+
+The Oversight Committee is made up of the Working Group chairs and Project Leads for active projects.
+
+Whereas the Steering Committee (SC) is more focused on long term strategic direction of the Foundation the Oversight Committee is more focused on tactical, day to day aspects of running the Foundation.
---
+++ docusaurus.config.js
@@ -0,0 +1,213 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  // Change to Site title
+  title: "GSF Oversight Committee",
+  // Change to site description
+  tagline: "",
+  // Change to site url
+  url: "https://oc.greensoftware.foundation/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Green-Software-Foundation",
+  // Change to GitHub repo name.
+  projectName: "oc",
+  deploymentBranch: "gh-pages",
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Change this to your repo.
+          editUrl: "https://github.com/Green-Software-Foundation/training/tree/main/",
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        gtag: {
+          trackingID: "G-J3XG8C4W57",
+        },
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5,
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        // Change to project name
+        title: "Oversight Committee",
+        logo: {
+          alt: "GSF logo",
+          src: "img/logo.svg",
+        },
+        items: [
+        {
+          type: 'localeDropdown',
+          position: 'left',
+        },  ],
+      },
+      footer: {
+        style: "dark",
+        links: [{
+          title: "Links",
+          items: [{
+            label: "Github",
+            href: "https://github.com/Green-Software-Foundation/training/",
+          },
+          {
+            label: "Website",
+            href: "https://learn.greensoftware.foundation",
+          },
+          ],
+        },
+        {
+          title: "LEGAL",
+          items: [{
+            label: "Trademark Policy",
+            href: "https://greensoftware.foundation/policy/trademark",
+          },
+          {
+            label: "Terms and Privacy Policy",
+            href: "https://greensoftware.foundation/policy/terms",
+          },
+          ],
+        },
+        {
+          title: "GSF Info",
+          items: [{
+            label: "Green Software Foundation",
+            href: "https://greensoftware.foundation",
+          },
+          {
+            label: "Twitter",
+            href: "https://twitter.com/gsfcommunity",
+          },
+          {
+            label: "Linkedin",
+            href: "https://www.linkedin.com/company/green-software-foundation/",
+          },
+          ],
+        },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Joint Development Foundation Projects, LLC, Green Software Foundation Series`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+      },
+      colorMode: {
+        disableSwitch: true,
+        defaultMode: "light",
+      },
+      metadata: [{
+        name: "twitter:card",
+        content: "summary",
+      },
+      {
+        name: "twitter:site",
+        content: "@gsfcommunity",
+      },
+      {
+        name: "twitter:title",
+        content: "Green Software Practitioner",
+      },
+      {
+        name: "twitter:description",
+        content: "If you want to make your software applications more sustainable and build a career in green software, then this training is for you",
+      },
+      {
+        name: "twitter:image",
+        content: "https://learn.greensoftware.foundation/img/og-image.png",
+      },
+      {
+        name: "twitter:creator",
+        content: "@gsfcommunity",
+      },
+      {
+        name: "og:title",
+        content: "Green Software Practitioner",
+      },
+      {
+        name: "og:description",
+        content: "If you want to make your software applications more sustainable and build a career in green software, then this training is for you",
+      },
+      {
+        name: "og:image",
+        content: "https://learn.greensoftware.foundation/img/og-image.png",
+      },
+      {
+        name: "og:url",
+        content: "https://learn.greensoftware.foundation/",
+      },
+      {
+        name: "og:site_name",
+        content: "Green Software Practitioner",
+      },
+      {
+        name: "og:type",
+        content: "website",
+      },
+
+      ]
+    }),
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [ // The course was originally posted under practitioner
+          {
+            to: '/introduction',
+            from: '/practitioner/introduction',
+          },
+          {
+            to: '/carbon-efficiency',
+            from: '/practitioner/carbon-efficiency',
+          },
+          {
+            to: '/energy-efficiency',
+            from: '/practitioner/energy-efficiency',
+          },
+          {
+            to: '/carbon-awareness',
+            from: '/practitioner/carbon-awareness',
+          },
+          {
+            to: '/hardware-efficiency',
+            from: '/practitioner/hardware-efficiency',
+          },
+          {
+            to: '/measurement',
+            from: '/practitioner/measurement',
+          },
+          {
+            to: '/climate-commitments',
+            from: '/practitioner/climate-commitments',
+          },
+          {
+            to: '/glossary', // Note there is a spelling correction here also
+            from: '/practitioner/glossery',
+          },
+        ]
+      },
+    ],
+  ],
+};
+
+module.exports = config;
